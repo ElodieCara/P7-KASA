0a98753dd02f3664e28a3846730f84c0db497412
ElodieCara <102527658+ElodieCara@users.noreply.github.com>
2023-04-19 11:11:45 +0200

    problem fixed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from './pages/About';
-import ErrorPage from './pages/Error404';
-import Home from './pages/Home';
-import ProductPage from './pages/ProductPage';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import ErrorPage from "./pages/Error404";
+import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/logement/:id" element={<ProductPage />} />
-                <Route path="*" element={<ErrorPage />} />
-            </Routes>
-        </BrowserRouter >
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logement/:id" element={<ProductPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
